b34acdbfdfe1f13f74e7269fffd669710662a430
vasfi <vgumus08@gmail.com>
2016-07-22 01:08:58 +0300

    sitemaps added

--- homePage/urls.py
+++ homePage/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
+from django.contrib.sitemaps.views import sitemap
+
+from .sitemaps import StaticViewSitemap
 from . import views
 
+sitemaps = {
+    'static' : StaticViewSitemap,
+}
+
 urlpatterns = [
-    url(r'^$',views.index, name='index')
+    url(r'^$',views.index, name='index'),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap')
 ]
--- silverbros/settings.py
+++ silverbros/settings.py
@@ -30,8 +30,12 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+SITE_ID = 1
+
 INSTALLED_APPS = [
     'homePage',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
